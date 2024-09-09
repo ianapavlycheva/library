ad3a59a275926414981fe5206fbc5d51f3d5111a
Iana Pavlycheva <ianapavlycheva@gmail.com>
2024-09-09 18:48:17 +1000

    Add display book function

--- javascript.js
+++ javascript.js
@@ -4,10 +4,29 @@ function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.isRead = isrRead;
+  this.isRead = isRead;
 }
 
 function addBook(title, author, pages, isRead) {
   const newBook = new Book(title, author, pages, isRead);
   myLibrary.push(newBook);
+}
+
+function displayBooks() {
+  const libraryDisplay = document.getElementById("library-display");
+  libraryDisplay.innerHTML = "";
+
+  myLibrary.forEach((book, index) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    bookCard.innerHTML = `
+      <h3>${book.title}</h3>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+      <p>Read: ${book.isRead ? "Yes" : "No"}</p>
+    `;
+
+    libraryDisplay.appendChild(bookCard);
+  });
 }
